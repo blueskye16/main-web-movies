839bb4f18d852fd5cfa81c0b86428d4d59d9ce76
blueskye16 <fabianuskevin147@gmail.com>
2024-05-30 22:58:07 +0700

    configuring file-loader for imgs

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,13 @@ import Alert from 'bootstrap/js/dist/alert'
 
 // or, specify which plugins you need:
 import { Tooltip, Toast, Popover } from 'bootstrap'
+
+import $ from "jquery";
+import moment from "moment";
+const api_key = "api_key=623353059db48f17ef551c106c4c2a4c";
+const baseUrl = "https://api.themoviedb.org/3/";
+const imgUrl = "https://image.tmdb.org/t/p/w500/";
+const tmdbMovie = "https://www.themoviedb.org/movie/";
+const formEl = document.querySelector(".card-body");
+let page = 1;
+
